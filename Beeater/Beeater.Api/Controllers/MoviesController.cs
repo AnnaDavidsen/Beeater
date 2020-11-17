e8c54ef94eb11efe5562908c86462b61cd79332a
annadavidsen <anna.m.davidsen@gmail.com>
2020-11-17 14:31:46 +0100

    add moviescontroller with getbyid and post methods

---
+++ Beeater/Beeater.Api/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using Beeater.Domain.Entities;
+using Beeater.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beeater.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private beeaterContext _context;
+        public MoviesController(beeaterContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovieById(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie != null)
+                return Ok(movie);
+
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostMovies([FromBody]IEnumerable<Movie> movies)
+        {
+            await _context.Movies.AddRangeAsync(movies);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
--- Beeater/Beeater.Api/Startup.cs
+++ Beeater/Beeater.Api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Beeater.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Beeater.Domain.Entities;
 
 namespace Beeater.Api
 {
@@ -25,6 +28,7 @@ namespace Beeater.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<beeaterContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
