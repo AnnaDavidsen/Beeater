3f9ea01224b903e537f92ed83d26ffb4aa62007a
annadavidsen <anna.m.davidsen@gmail.com>
2020-11-17 16:18:21 +0100

    add bookingscontroller

---
+++ Beeater/Beeater.Api/Controllers/BookingsController.cs
@@ -0,0 +1,24 @@
+﻿using Beeater.Domain.Entities;
+using Beeater.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beeater.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : CommonController<Booking>
+    {
+        public BookingsController(beeaterContext context)
+            : base(context)
+        {
+
+        }
+        // GetAll, GetById and Post are located in CommonController class
+
+    }
+}
