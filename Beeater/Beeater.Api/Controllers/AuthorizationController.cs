d92a9b0dfbd825ea3d780b688aecf28ad69397b6
nissefar <nikolajharder@live.dk>
2020-11-18 12:33:54 +0100

    authorization & employeee controller added

---
+++ Beeater/Beeater.Api/Controllers/AuthorizationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Beeater.Domain.Entities;
+using Beeater.Persistence;
+
+namespace Beeater.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : CommonController<Authorization>
+    {
+        public AuthorizationController(beeaterContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
---
+++ Beeater/Beeater.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Beeater.Domain.Entities;
+using Beeater.Persistence;
+
+namespace Beeater.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : CommonController<Employee>
+    {
+        public EmployeeController(beeaterContext context)
+            : base(context)
+        {
+
+        }
+    }
+
+
+}
