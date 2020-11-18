3c1e46d83f5e199a60ba8b08f9a224dac8732262
nissefar <nikolajharder@live.dk>
2020-11-18 11:39:22 +0100

    hej

--- Beeater/Beeater.Api/Controllers/ShowsController.cs
+++ Beeater/Beeater.Api/Controllers/ShowsController.cs
@@ -2,6 +2,7 @@
 using Beeater.Persistence;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,35 @@ namespace Beeater.Api.Controllers
             : base(context)
         {
 
+        }
+        [HttpGet("movieid/{movieid}")]
+        public async Task<ActionResult<IEnumerable<Show>>> GetShowByMovieTitle(int movieId)
+        {
+            var shows = await _context.Shows
+                .Where(s => s.MovieId == movieId)
+                .ToListAsync();
+
+            return Ok(shows);
+        }
+
+        [HttpGet("title/{title}")]
+        public async Task<ActionResult<IEnumerable<Show>>> GetShowByMovieTitle(string title)
+        {
+            var shows = await _context.Shows
+                .Where(s => _context.Movies
+                .Any(m => title == m.Title))
+                .ToListAsync();
+
+            return Ok(shows);
+        }
+        [HttpGet("theater/{theaterId}")]
+        public async Task<ActionResult<IEnumerable<Show>>> GetShowsByTheaterId(int theaterId)
+        {
+            var shows = await _context.Shows
+                .Where(s => s.TheaterId == theaterId)
+                .ToListAsync();
+
+            return Ok(shows);
         }
     }
 }
