a78b3a37a01bd4786e928f5941b0f6c7d4635053
nissefar <nikolajharder@live.dk>
2020-11-19 11:59:25 +0100

    post theater

--- Beeater/Beeater.Api/Controllers/TheatersController.cs
+++ Beeater/Beeater.Api/Controllers/TheatersController.cs
@@ -2,6 +2,7 @@
 using Beeater.Persistence;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,31 @@ namespace Beeater.Api.Controllers
         {
 
         }
+
+        [HttpPost("seats")]
+        public async Task<ActionResult> Post([FromBody] JObject theaterAndRows)
+        {
+            Theater theater = theaterAndRows["theater"].ToObject<Theater>();
+            int[] rows = theaterAndRows["rows"].ToObject<int[]>();
+            _context.Theaters.Add(theater);
+            await _context.SaveChangesAsync();
+
+            int row = 0;
+            foreach (var seats in rows)
+            {
+                row++;
+                for (int i = 1; i <= seats; i++)
+                {
+                    _context.Seats.Add(new Seat { Number = i, Row = row, TheaterId = theater.Id });
+
+                }
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        
     }
 }
