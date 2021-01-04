5bb269301832eb52858a460a9c9630bcce24e3b3
annadavidsen <anna.m.davidsen@gmail.com>
2021-01-04 10:11:48 +0100

    remove connection string from context

--- Beeater/Beeater.Persistence/beeaterContext.cs
+++ Beeater/Beeater.Persistence/beeaterContext.cs
@@ -33,11 +33,7 @@ namespace Beeater.Persistence
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=beeaterserver.database.windows.net;Initial Catalog=beeater;User ID=stumpedumpe;Password=~u/Z`4_cC&q8D:u`G*WM;");
-            }
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ Beeater/Beeater.Test/Controllers/MoviesController_Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Beeater.Test.Controllers
+{
+    public class MoviesController_Test
+    {
+    }
+}
